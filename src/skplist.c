b552b76be4a70fb93a2dad969511a200148ce1c0
zzzj <1152092752@qq.com>
2021-04-21 18:43:35 +0800

    更新skpList

--- src/skplist.c
+++ src/skplist.c
@@ -18,12 +18,20 @@ void zslRepr(zskiplist *zsl) {
 
         while (node != NULL) {
             if (node->level[i].forward != NULL) {
-                printf(" %.0f ", node->level[i].forward->score);
+                printf("score =  %.0f , span = %d | ", node->level[i].forward->score, node->level[i].span);
             }
             node = node->level[i].forward;
         }
 
         printf("\n");
+    }
+}
+
+void nodeRepr(struct zskiplistNode *node) {
+    if (node == NULL) {
+        printf("node is null \n");
+    } else {
+        printf("score =  %.0f  , value = %d \n", node->score, *((int *) node->obj));
     }
 }
 
@@ -61,7 +69,7 @@ zskiplist *zslCreate(void) {
 
 zskiplistNode *zslInsert(zskiplist *zsl, double score, void *obj) {
     zskiplistNode *update[ZSKIPLIST_MAXLEVEL];
-    unsigned int rank[ZSKIPLIST_MAXLEVEL];
+    unsigned int rank[ZSKIPLIST_MAXLEVEL] = {0};
 
     // 先看是否已经存在相同的分值,存在则更新obj
 
@@ -69,17 +77,15 @@ zskiplistNode *zslInsert(zskiplist *zsl, double score, void *obj) {
     for (int i = zsl->level - 1; i >= 0; --i) {
         zskiplistNode *node = zsl->header;
 
-        rank[i] = (i == zsl->level - 1) ? 0 : rank[i + 1];
-
         while (node != NULL && node->level[i].forward) {
             // todo 如果score相同并且obj的内容相同,那么这次插入失败 , 目前仅判断score是否相同
             if (node->level[i].forward->score == score) {
                 return NULL;
-            } else if (node->level[i].forward->score > score) {
-                node = node->level[i].forward;
-                // node->level[i].forward->score < score
-            } else {
+            } else if (node->level[i].forward->score < score) {
                 rank[i] += node->level[i].span;
+                node = node->level[i].forward;
+                // node->level[i].forward->score > score
+            } else {
                 break;
             }
         }
@@ -87,8 +93,6 @@ zskiplistNode *zslInsert(zskiplist *zsl, double score, void *obj) {
     }
 
     int lev = zslRandomLevel();
-
-    printf("lev = %d , score = %.0f \n", lev, score);
 
     if (lev > zsl->level) {
         for (int i = zsl->level; i < lev; ++i) {
@@ -100,15 +104,87 @@ zskiplistNode *zslInsert(zskiplist *zsl, double score, void *obj) {
 
     zskiplistNode *newNode = zslCreateNode(lev, obj, score);
 
+    zsl->length++;
+
     for (int i = 0; i < lev; ++i) {
         zskiplistNode *forward = update[i]->level[i].forward;
         update[i]->level[i].forward = newNode;
+        update[i]->level[i].span = zsl->length - rank[i];
         newNode->level[i].forward = forward;
     }
 
-    zsl->level++;
+    // update[0]就是第一层
+    newNode->backward = update[0] == zsl->header ? NULL : update[0];
+
+    if (newNode->level[0].forward) {
+        newNode->level[0].forward->backward = newNode;
+    } else {
+        // 没有下一个元素了,当前元素就是第一层的最后一个元素
+        zsl->tail = newNode;
+    }
 
     return newNode;
+}
+
+// 如果给定的分值范围包含在跳跃表的分值范围之内,返回1,否则返回0
+int zslIsInRange(zskiplist *zsl, zrangespec *range) {
+    if (range->min > range->max
+        || (range->min == range->max && (range->maxex || range->minex))) {
+        return 0;
+    }
+
+    if (zsl->header->level[0].forward == NULL) {
+        return 0;
+    }
+
+    zskiplistNode *head = zsl->header->level[0].forward;
+
+    // zsl列表最小的元素的score大于range.max
+    if (head->score >= range->max) {
+        // 如果max包含,那么返回判断是否是 == , 否则返回0
+        return !range->maxex ? head->score == range->max : 0;
+    }
+
+    // zsl列表最大的元素的score小于range.min
+    if (zsl->tail->score <= range->min) {
+        return !range->minex ? zsl->tail->score == range->min : 0;
+    }
+
+    return 1;
+}
+
+// 返回 zsl 中第一个分值符合 range 中指定范围的节点
+// 如果 zsl 中没有符合范围的节点,返回 NULL
+zskiplistNode *zslFirstInRange(zskiplist *zsl, zrangespec *range) {
+
+    // 1. 超出范围,直接返回
+    if (!zslIsInRange(zsl, range)) {
+        return NULL;
+    }
+
+    // 2. 遍历skpList
+    for (int i = zsl->level - 1; i >= 0; --i) {
+        zskiplistNode *node = zsl->header->level[i].forward;
+
+        while (node) {
+            // 找到第一个比range.min大的元素
+            if (node->score >= range->min) {
+                if (node->score == range->min) {
+                    // 包含最小值
+                    if (!range->minex) {
+                        return node;
+                    }
+                    // >
+                } else {
+                    return node;
+                }
+            }
+            node = node->level[i].forward;
+        }
+    }
+
+    // redisLog
+    exit(1);
 }
 
 int main() {
@@ -123,4 +199,11 @@ int main() {
     zslInsert(zsl, 1, &num1);
 
     zslRepr(zsl);
+
+    zrangespec range = {
+            1, 2, 0, 0
+    };
+
+    zskiplistNode *node = zslFirstInRange(zsl, &range);
+    nodeRepr(node);
 }
--- src/skplist.h
+++ src/skplist.h
@@ -37,12 +37,25 @@ typedef struct zskiplist {
 
 } zskiplist;
 
+// 表示开区间/闭区间范围的结构
+typedef struct {
+
+    // 最小值和最大值
+    double min, max;
+
+    // 指示最小值和最大值是否*不*包含在范围之内
+    // 值为 1 表示不包含，值为 0 表示包含
+    int minex, maxex;
+} zrangespec;
+
 
 zskiplist *zslCreate(void);
 
 zskiplistNode *zslCreateNode(int level, void *obj, double score);
 
 zskiplistNode *zslInsert(zskiplist *zsl, double score, void *obj);
+
+zskiplistNode *zslFirstInRange(zskiplist *zsl, zrangespec *range);
 
 void zslFree(zskiplist *zsl);
 
